c627ffb5bd7e7a7aaf9624cebf9df623e2c9e90e
KatiFerrari <katielidasilvaferrari26@gmail.com>
2023-11-26 22:06:00 -0400

    atualizado-26-11
    Criei a página cadastro e copie o código para cadastrar do último projeto e alterei as propriedades do objeto. Copiei a função de criar o card e ser renderizado nas páginas home, detalhe

---
+++ src/Cadastro/cadastrar.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+export default function Cadastar() {
+    const listaLocalStorage = JSON.parse(localStorage.getItem("Lista"));
+   const [NomeDaMusica, setNomeMusc ] = useState("");
+   const [Letra, setLetra ] = useState("");
+   const [Artista, setCantor] = useState("");
+   const [Link, setLink] = useState("");
+   const [lista, setLista ] = useState(listaLocalStorage || []);
+   const [id,setId] = useState(listaLocalStorage[listaLocalStorage.length - 1]?.id + 1 || 1);
+
+   useEffect(() => {
+    localStorage.setItem("Lista", JSON.stringify(lista));
+  }, [lista]);
+
+    const salvar =(e) =>{
+        e.preventDefault();
+        setLista([...lista, {
+                tipo: tipo, marca:marca, preco:preco, img:img,
+
+                id: id
+        }]);
+        setId(id + 1);
+        setTipo("");
+        setMarca("");
+        setPreco("");
+        setImg("");
+        console.log(id)
+    }}
--- src/Home/main.jsx
+++ src/Home/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Home'
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
      <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path='/detalhe/:id' element={<Detalhe />}></Route>
+       
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
 )
-
