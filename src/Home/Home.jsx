00c9bd9b9349a8f2f5b97b812fa80921ca963652
KatiFerrari <katielidasilvaferrari26@gmail.com>
2023-12-01 14:32:54 -0400

    atualizado-01-12
    Inserindo um navbar com os links do bootstrap e criando as rotas do reader com a home. E criando o map com suas respectivas propriedades para mapear.

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,7 +1,12 @@
+import Header from './componentes/header';
 import { useState } from "react";
 export default function Home(){
     return(
-
+      <div>
+        <Header/>
+        <h1>{videos}</h1>
         <h1>PTAC 2 - PROJETO FINAL</h1>
+      </div>
+        
      );
 }
